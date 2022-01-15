ccd7d37ad6bd6b102052de57c7f9ea7effcb01ab
leondragan <kevinlee2821@gmail.com>
2022-01-14 18:19:50 -0800

    song struct; can't serialize

--- programs/somos-solana/src/lib.rs
+++ programs/somos-solana/src/lib.rs
@@ -1,21 +1,23 @@
 use anchor_lang::prelude::*;
 
-declare_id!("7PPNqpdvgetTjxaMnMR9ADqm2gSw8KwEpmKkRrq3ACNs");
+declare_id!("AgxH9tmJsyVHiN7c6mMwkPh77dzgQxWQv1o1GgeSHFtN");
 
 #[program]
 pub mod somos_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, bump: u8, binary: u128) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, bump: u8, title: String, one: Song, two: Song) -> ProgramResult {
         let music_account = &mut ctx.accounts.music_account;
-        music_account.binary = binary;
+        music_account.title = title;
+        music_account.one = one;
+        music_account.two = two;
         music_account.bump = bump;
         Ok(())
     }
 
-    pub fn update(ctx: Context<Update>, binary: u128) -> ProgramResult {
+    pub fn update(ctx: Context<Update>, title: String) -> ProgramResult {
         let music_account = &mut ctx.accounts.music_account;
-        music_account.binary = binary;
+        music_account.title = title;
         Ok(())
     }
 }
@@ -23,8 +25,8 @@ pub mod somos_solana {
 #[derive(Accounts)]
 #[instruction(bump: u8)]
 pub struct Initialize<'info> {
-    #[account(init, seeds = [b"somos_seed".as_ref()], bump = bump, payer = user)]
-    music_account: Account<'info, Music>,
+    #[account(init, seeds = [b"somos_seed".as_ref()], bump = bump, payer = user, space = 16 + 16)]
+    music_account: Account<'info, Project>,
     #[account(mut)]
     user: Signer<'info>,
     system_program: Program<'info, System>,
@@ -33,12 +35,20 @@ pub struct Initialize<'info> {
 #[derive(Accounts)]
 pub struct Update<'info> {
     #[account(mut, seeds = [b"somos_seed".as_ref()], bump = music_account.bump)]
-    music_account: Account<'info, Music>,
+    music_account: Account<'info, Project>,
 }
 
 #[account]
-#[derive(Default)]
-pub struct Music {
-    binary: u128,
-    bump: u8,
+pub struct Project {
+    pub title: String,
+    pub one: Song,
+    pub two: Song,
+    // used to persist data with unique seed + .bump
+    pub bump: u8,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct Song {
+    pub title: String,
+    pub encoded_wav: String,
 }
--- tests/somos-solana.js
+++ tests/somos-solana.js
@@ -5,51 +5,55 @@ describe("somos-solana", () => {
     // Configure the client
     const provider = anchor.Provider.env();
     anchor.setProvider(provider);
-
+    // fetch program
     const program = anchor.workspace.SomosSolana;
-
-    let musicAccount, bump;
+    // derive pda key with bump
+    let pdaPublicKey, bump;
     before(async () => {
-        [musicAccount, bump] =
+        [pdaPublicKey, bump] =
             await anchor.web3.PublicKey.findProgramAddress(
                 [Buffer.from("somos_seed")],
                 program.programId
             );
     });
-
-    let fakeBinaryMusicData = new anchor.BN(1234)
-
-    it("initializes music account with binary data & bump", async () => {
-        await program.rpc.initialize(new anchor.BN(bump), fakeBinaryMusicData, {
+    // data
+    let title = "EP01";
+    let song01 = {
+        title: "Track01",
+        encodedWav: "base64_str_01"
+    };
+    // init
+    it("initializes music account with data & bump", async () => {
+        await program.rpc.initialize(new anchor.BN(bump), title, song01, song01, {
             accounts: {
                 user: provider.wallet.publicKey,
-                musicAccount: musicAccount,
+                musicAccount: pdaPublicKey,
                 systemProgram: anchor.web3.SystemProgram.programId,
             }
         });
-        let actual = await program.account.music.fetch(
-            musicAccount
+        let actual = await program.account.project.fetch(
+            pdaPublicKey
         );
         console.log(actual)
-        assert.equal(actual.binary.toNumber(), fakeBinaryMusicData.toNumber());
+        assert.equal(actual.title.toString(), title.toNumber());
         assert.equal(actual.bump, bump);
     });
 
-    let moreFakeBinaryMusicData = new anchor.BN(4321)
-
+    // data
+    let newTitle = "EP_01"
+    // update
     it("updates music account with new binary data", async () => {
-        await program.rpc.update(moreFakeBinaryMusicData, {
+        await program.rpc.update(newTitle, {
             accounts: {
-                musicAccount: musicAccount
+                musicAccount: pdaPublicKey
             }
         })
-        let actual = await program.account.music.fetch(
-            musicAccount
+        let actual = await program.account.project.fetch(
+            pdaPublicKey
         )
         console.log(actual)
-        assert.equal(actual.binary.toNumber(), moreFakeBinaryMusicData.toNumber());
+        assert.equal(actual.title.toString(), newTitle.toString());
         assert.equal(actual.bump, bump);
     });
-
 
 });
