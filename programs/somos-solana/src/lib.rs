649ffea1a4f2f1eaab2d2a52e0856e1e511a6a8b
leondragan <kevinlee2821@gmail.com>
2022-01-19 17:21:49 -0800

    dropped partion strategy in-favor of web2 host

--- programs/somos-solana/src/lib.rs
+++ programs/somos-solana/src/lib.rs
@@ -6,25 +6,6 @@ declare_id!("AgxH9tmJsyVHiN7c6mMwkPh77dzgQxWQv1o1GgeSHFtN");
 pub mod somos_solana {
     use super::*;
 
-    pub fn initialize_partition_one(
-        ctx: Context<InitializeOne>,
-        bump: u8,
-    ) -> ProgramResult {
-        let partition = &mut ctx.accounts.partition;
-        partition.bump = bump;
-        Ok(())
-    }
-
-    pub fn initialize_partition_two(
-        ctx: Context<InitializeTwo>,
-        bump: u8,
-    ) -> ProgramResult {
-        let partition = &mut ctx.accounts.partition;
-        partition.bump = bump;
-        partition.authority = ctx.accounts.user.key();
-        Ok(())
-    }
-
     pub fn initialize_ledger(
         ctx: Context<InitializeLedger>,
         bump: u8,
@@ -52,55 +33,6 @@ pub mod somos_solana {
             ledger,
         )
     }
-
-    pub fn update(
-        ctx: Context<Update>,
-        data_one: String,
-        data_two: String,
-    ) -> ProgramResult {
-        let partition_one = &mut ctx.accounts.partition_one;
-        let partition_two = &mut ctx.accounts.partition_two;
-        partition_one.data = data_one;
-        partition_two.data = data_two;
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-#[instruction(bump: u8)]
-pub struct InitializeOne<'info> {
-    #[account(init, seeds = [b"hemingway".as_ref()], bump = bump, payer = user, space = 10240)]
-    pub partition: Account<'info, Partition>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-#[instruction(bump: u8)]
-pub struct InitializeTwo<'info> {
-    #[account(init, seeds = [b"miller".as_ref()], bump = bump, payer = user, space = 10240)]
-    pub partition: Account<'info, Partition>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut, seeds = [b"hemingway".as_ref()], bump = partition_one.bump)]
-    pub partition_one: Account<'info, Partition>,
-    #[account(mut, seeds = [b"miller".as_ref()], bump = partition_two.bump, has_one = authority)]
-    pub partition_two: Account<'info, Partition>,
-    pub authority: Signer<'info>,
-}
-
-#[account]
-pub struct Partition {
-    pub data: String,
-    pub bump: u8,
-    // only init user can access
-    pub authority: Pubkey,
 }
 
 #[derive(Accounts)]
@@ -124,7 +56,6 @@ pub struct InitializeLedger<'info> {
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
-
 
 #[account]
 pub struct Ledger {
--- tests/somos-solana.js
+++ tests/somos-solana.js
@@ -32,24 +32,6 @@ describe("somos-solana", () => {
     }
 
     // derive pda key
-    let pdaOnePublicKey, bumpOne;
-    before(async () => {
-        [pdaOnePublicKey, bumpOne] =
-            await anchor.web3.PublicKey.findProgramAddress(
-                [Buffer.from("hemingway")],
-                program.programId
-            );
-    });
-    // derive pda key
-    let pdaTwoPublicKey, bumpTwo;
-    before(async () => {
-        [pdaTwoPublicKey, bumpTwo] =
-            await anchor.web3.PublicKey.findProgramAddress(
-                [Buffer.from("miller")],
-                program.programId
-            );
-    });
-    // derive pda key
     let pdaLedgerPublicKey, bumpLedger;
     before(async () => {
         [pdaLedgerPublicKey, bumpLedger] =
@@ -57,41 +39,6 @@ describe("somos-solana", () => {
                 [Buffer.from("hancock")],
                 program.programId
             );
-    });
-    // data
-    let dataOne = "12345"
-    let dataTwo = "678910"
-    // init
-    it("initializes partition one with bump", async () => {
-        let _provider = await createUser()
-        let _program = programForUser(_provider)
-        console.log(_provider.key.publicKey)
-        await _program.rpc.initializePartitionOne(new anchor.BN(bumpOne), {
-            accounts: {
-                user: _provider.key.publicKey,
-                partition: pdaOnePublicKey,
-                systemProgram: anchor.web3.SystemProgram.programId,
-            }
-        });
-        let actualOne = await _program.account.partition.fetch(
-            pdaOnePublicKey
-        );
-        console.log(actualOne)
-    });
-    // init
-    it("initializes partition two with bump", async () => {
-        console.log(provider.wallet.publicKey)
-        await program.rpc.initializePartitionTwo(new anchor.BN(bumpTwo), {
-            accounts: {
-                user: provider.wallet.publicKey,
-                partition: pdaTwoPublicKey,
-                systemProgram: anchor.web3.SystemProgram.programId,
-            }
-        });
-        let actualTwo = await program.account.partition.fetch(
-            pdaTwoPublicKey
-        );
-        console.log(actualTwo)
     });
     // init
     it("initializes ledger with bump", async () => {
@@ -150,24 +97,6 @@ describe("somos-solana", () => {
         } catch (error) {
             console.log(error)
         }
-    });
-    // update
-    it("updates both partitions with data", async () => {
-        await program.rpc.update(dataOne, dataTwo, {
-            accounts: {
-                partitionOne: pdaOnePublicKey,
-                partitionTwo: pdaTwoPublicKey,
-                authority: provider.wallet.publicKey
-            }
-        });
-        let actualOne = await program.account.partition.fetch(
-            pdaOnePublicKey
-        );
-        let actualTwo = await program.account.partition.fetch(
-            pdaTwoPublicKey
-        );
-        console.log(actualOne)
-        console.log(actualTwo)
     });
 
 });
