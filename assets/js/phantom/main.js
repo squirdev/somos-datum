191593e0e2eddaf343088f08f908b59e65d2cbfa
leondragan <kevinlee2821@gmail.com>
2022-01-27 22:17:26 -0800

    towards controlled state

--- assets/js/phantom/main.js
+++
@@ -1,8 +0,0 @@
-/*! https://docs.phantom.app/ */
-// validate phantom install
-import {getPhantom} from "./index.js";
-
-// connect
-app.ports.connectSender.subscribe(async function () {
-    await getPhantom()
-});
