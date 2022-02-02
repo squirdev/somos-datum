f64c75e3c09e9a5bdd899852463de826b3e20467
leondragan <kevinlee2821@gmail.com>
2022-02-01 17:05:49 -0800

    devent deploy

--- assets/js/anchor/config.js
+++ assets/js/anchor/config.js
@@ -6,7 +6,7 @@ export const programID = new web3.PublicKey(idl.metadata.address);
 export const ACCOUNT_SEED = "hancock"
 export const BOSS = new web3.PublicKey("DLXRomaskStghSHAyoFZMKnFk1saLYDhYggW25Ze4jug")
 
-const localnet = "http://127.0.0.1:8899";
-// const devnet = clusterApiUrl("devnet");
+// const localnet = "http://127.0.0.1:8899";
+const devnet = web3.clusterApiUrl("devnet");
 // const mainnet = clusterApiUrl("mainnet-beta");
-export const network = localnet;
+export const network = devnet;
