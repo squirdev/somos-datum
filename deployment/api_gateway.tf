0c8b79675fd864d972120d2b8a14ef5f6dd68321
leondragan <kevinlee2821@gmail.com>
2022-02-01 16:00:31 -0800

    api gateway

---
+++ deployment/api_gateway.tf
@@ -0,0 +1,36 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = "SomosDownload"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part = "resource"
+  parent_id = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "post" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.post.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.download.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.download.function_name
+  principal = "apigateway.amazonaws.com"
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:us-west-2:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.post.http_method}${aws_api_gateway_resource.resource.path}"
+}
--- download/src/main/js/index.js
+++ download/src/main/js/index.js
@@ -6,7 +6,7 @@ exports.handler = async function (event, context) {
     console.log('## ENVIRONMENT VARIABLES: ' + serialize(process.env))
     console.log('## CONTEXT: ' + serialize(context))
     console.log('## EVENT: ' + serialize(event))
-    const _verified = verify(event)
+    const _verified = verify(JSON.parse(event.body))
     try {
         return formatResponse(serialize({verified: _verified}))
     } catch (error) {
