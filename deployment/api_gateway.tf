fef90b36b6372f5e1c13da24927f07a00f650704
leondragan <kevinlee2821@gmail.com>
2022-02-01 16:06:25 -0800

    api gateway deploy

--- deployment/api_gateway.tf
+++ deployment/api_gateway.tf
@@ -34,3 +34,15 @@ resource "aws_lambda_permission" "apigw_lambda" {
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:us-west-2:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.post.http_method}${aws_api_gateway_resource.resource.path}"
 }
+
+resource "aws_api_gateway_deployment" "deploy" {
+  depends_on = [
+    aws_api_gateway_resource.resource
+  ]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name = lower(terraform.workspace)
+  variables = {
+    // force deployment
+    "answer" = aws_s3_bucket_object.lambda.etag
+  }
+}
