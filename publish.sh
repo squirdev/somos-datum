40cddd5cebb5c95a796dd926d81ed3c2353e0c1f
leondragan <kevinlee2821@gmail.com>
2022-02-06 11:50:32 -0800

    publish sync images

--- publish.sh
+++ publish.sh
@@ -14,6 +14,7 @@ echo "Building Elm Assets..."
 ./build.sh
 
 echo "Publishing New Assets..."
+aws s3 sync assets/images/ $BUCKET/images/ --profile tap-in
 aws s3 cp assets/index.html $BUCKET --profile tap-in
 aws s3 cp assets/elm.min.js $BUCKET --profile tap-in
 aws s3 cp assets/css/ $BUCKET/css/ --recursive --profile tap-in
