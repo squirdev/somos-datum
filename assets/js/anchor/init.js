5c74cd04490e145cc22b1fe1f915f5d9c3083419
leondragan <kevinlee2821@gmail.com>
2022-02-06 15:19:23 -0800

    init @ n=10 price=0.025

--- assets/js/anchor/init.js
+++ assets/js/anchor/init.js
@@ -3,7 +3,8 @@ import {getCurrentState} from "./state";
 
 export async function init(program, provider, statePublicKey, bump, user) {
     try {
-        await program.rpc.initializeLedger(new BN(bump), {
+        const price = 0.025 * web3.LAMPORTS_PER_SOL
+        await program.rpc.initializeLedgerOne(new BN(bump), new BN(100), new BN(price), {
             accounts: {
                 user: provider.wallet.publicKey,
                 ledger: statePublicKey,
