2f54d565f3c45363672fd568cb5077d01e58374f
leondragan <kevinlee2821@gmail.com>
2022-02-10 09:44:57 -0800

    publish fonts

--- publish.sh
+++ publish.sh
@@ -18,6 +18,7 @@ aws s3 sync assets/images/ $BUCKET/images/ --profile tap-in
 aws s3 cp assets/index.html $BUCKET --profile tap-in
 aws s3 cp assets/elm.min.js $BUCKET --profile tap-in
 aws s3 cp assets/css/ $BUCKET/css/ --recursive --profile tap-in
+aws s3 cp assets/fonts/ $BUCKET/fonts/ --recursive --profile tap-in
 aws s3 cp assets/js/bundle.js $BUCKET/js/ --profile tap-in
 
 echo "Invalidating CloudFront Cache..."
