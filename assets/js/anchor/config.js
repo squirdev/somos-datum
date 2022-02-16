ace2d07b071450634eecd40566462c4e34a8f27f
leondragan <kevinlee2821@gmail.com>
2022-02-16 09:16:14 -0800

    client pass seed to init rpc method

--- assets/js/anchor/config.js
+++ assets/js/anchor/config.js
@@ -3,10 +3,10 @@ import idl from "./idl.json";
 
 export const preflightCommitment = "processed";
 export const programID = new web3.PublicKey(idl.metadata.address);
-export const ACCOUNT_SEED = "hancock"
+export const ACCOUNT_SEED = Buffer.from("hancockhancockha");
 export const BOSS = new web3.PublicKey("DLXRomaskStghSHAyoFZMKnFk1saLYDhYggW25Ze4jug")
 
-// const localnet = "http://127.0.0.1:8899";
+const localnet = "http://127.0.0.1:8899";
 // const devnet = web3.clusterApiUrl("devnet");
-const mainnet = web3.clusterApiUrl("mainnet-beta");
-export const network = mainnet;
+// const mainnet = web3.clusterApiUrl("mainnet-beta");
+export const network = localnet;
--- assets/js/anchor/init.js
+++ assets/js/anchor/init.js
@@ -1,10 +1,11 @@
 import {web3, BN} from "@project-serum/anchor";
 import {getCurrentState} from "./state";
+import {ACCOUNT_SEED} from "./config";
 
 export async function init(program, provider, statePublicKey, bump, user) {
     try {
         const price = 0.025 * web3.LAMPORTS_PER_SOL
-        await program.rpc.initializeLedgerOne(new BN(bump), new BN(100), new BN(price), {
+        await program.rpc.initializeLedger(ACCOUNT_SEED, new BN(bump), new BN(100), new BN(price), {
             accounts: {
                 user: provider.wallet.publicKey,
                 ledger: statePublicKey,
