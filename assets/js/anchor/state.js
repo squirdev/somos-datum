142594222140279052e38652917d4412a9221888
leondragan <kevinlee2821@gmail.com>
2022-03-16 20:48:13 -0700

    encode .more before sending

--- assets/js/anchor/state.js
+++ assets/js/anchor/state.js
@@ -1,26 +1,31 @@
 // get ledger state
-export async function getCurrentState(program, ledger, user) {
+export async function getCurrentState(program, ledger, userJson) {
     try {
         // fetch state
         const _state = await program.account.ledger.fetch(ledger);
+        // decode user
+        const user = JSON.parse(userJson);
+
         // simplify for codec
         function simplify() {
             // TODO; send pubkey to elm
             const _owners = _state.owners.map(_publicKey => _publicKey.toString());
+            const more = {
+                price: Number(_state.price.toString()),
+                resale: _state.resale, // not a BN type
+                originalSupplyRemaining: Number(_state.originalSupplyRemaining.toString()),
+                owners: _owners,
+                user: user.more
+            };
             return {
                 user: user.user,
-                more: {
-                    price: Number(_state.price.toString()),
-                    resale: _state.resale, // not a BN type
-                    originalSupplyRemaining: Number(_state.originalSupplyRemaining.toString()),
-                    owners: _owners,
-                    user: user.more
-                }
+                more: JSON.stringify(more)
             }
         }
-        const state = simplify()
+
+        const state = simplify();
         // encode
-        const simplified = JSON.stringify(state)
+        const simplified = JSON.stringify(state);
         // send state to elm
         app.ports.getCurrentStateSuccessListener.send(simplified);
         // log success
