080bd401343fc190adbe575beeeaf06d383db812
leondragan <kevinlee2821@gmail.com>
2022-04-24 12:25:29 -0300

    devnet deploy

--- assets/js/anchor/config.js
+++ assets/js/anchor/config.js
@@ -6,7 +6,7 @@ export const ACCOUNT_SEED_01 = Buffer.from("hancockhancockha");
 export const ACCOUNT_SEED_02 = Buffer.from("robsonrobsonrobs");
 export const BOSS = new web3.PublicKey("DLXRomaskStghSHAyoFZMKnFk1saLYDhYggW25Ze4jug")
 
-const localnet = "http://127.0.0.1:8899";
-// const devnet = web3.clusterApiUrl("devnet");
+// const localnet = "http://127.0.0.1:8899";
+const devnet = web3.clusterApiUrl("devnet");
 // const mainnet = web3.clusterApiUrl("mainnet-beta");
-export const network = localnet;
+export const network = devnet;
